6bfe6d028fc4244c9bb81b84d85b102b505a0553
Natdanai Kanyakoon <moss10612@gmail.com>
2022-12-11 22:08:49 +0700

    updated css

--- src/components/container/Footer/Footer.jsx
+++ src/components/container/Footer/Footer.jsx
@@ -1,37 +1,34 @@
-import React from 'react';
-import "./Footer.scss"
-import { socialIcons } from '../../../Data';
-import { motion } from 'framer-motion';
+import React from "react";
+import "./Footer.scss";
+import { socialIcons } from "../../../Data";
+import { motion } from "framer-motion";
 
 const Footer = () => {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{
-        opacity:
-          1
-      }}
-      transition={{duration: 1.5}}
-      className='footer'
-    >
-      <div className="copyright">
-        <p>Copyright&copy;2022 All rights reserved.Made by <span>Ye Lin Ko</span></p>
-      </div>
-      <div className="followMe">
-        <h4>Follow Me</h4>
-        <div className="stick"></div>
-        <div className="social_icons">
-            {socialIcons.map((socialIcon, index) => {
-            return (
-              <div key={index} >
-                {socialIcon}
-              </div>
-            )
-          })}
-          </div>
-      </div>
-    </motion.div>
-  )
-}
+   return (
+      <motion.div
+         initial={{ opacity: 0 }}
+         whileInView={{
+            opacity: 1,
+         }}
+         transition={{ duration: 1.5 }}
+         className="footer"
+      >
+         <div className="copyright">
+            <p>
+               Copyright&copy;2022 All rights reserved.Made by <span>Natdanai</span>
+            </p>
+         </div>
+         <div className="followMe">
+            <h4>Follow Me</h4>
+            <div className="stick"></div>
+            <div className="social_icons">
+               {socialIcons.map((socialIcon) => {
+                  return <div key={socialIcon.id}>{socialIcon.icon}</div>;
+               })}
+            </div>
+         </div>
+      </motion.div>
+   );
+};
 
-export default Footer
+export default Footer;
